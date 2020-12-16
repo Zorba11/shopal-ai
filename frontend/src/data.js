cee76ea7ad410b7d05e7c24a1efba03acb7c606a
Zorba11 <zorbathegreek111@gmail.com>
2020-12-16 17:59:18 +0530

    template created, template implemented in create-react-app

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: 1,
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: 2,
+            name: 'Armani Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 80,
+            brand: 'Armani',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: 3,
+            name: 'NoBrand Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 150,
+            brand: 'NoBrand',
+            rating: 5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: 4,
+            name: 'Spike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 120,
+            brand: 'Spike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: 5,
+            name: 'ShivNaresh Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 100,
+            brand: 'ShivNaresh',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: 6,
+            name: 'Addidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p6.jpg',
+            price: 60,
+            brand: 'Addidas',
+            rating: 4,
+            numReviews: 10,
+            description: 'hight quality product'
+        }
+    ]
+}
+
+export default data;
