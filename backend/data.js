7ae889304e1edb87991601aa111107ea65bb413f
Zorba11 <zorbathegreek111@gmail.com>
2020-12-18 13:00:27 +0530

    created node backend

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 80,
+            countInstock: 10,
+            brand: 'Nike',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: '2',
+            name: 'Armani Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 80,
+            countInstock: 20,
+            brand: 'Armani',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: '3',
+            name: 'NoBrand Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 150,
+            countInstock: 14,
+            brand: 'NoBrand',
+            rating: 5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: '4',
+            name: 'Spike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 120,
+            countInstock: 9,
+            brand: 'Spike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: '5',
+            name: 'ShivNaresh Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 100,
+            countInstock: 11,
+            brand: 'ShivNaresh',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: '6',
+            name: 'Addidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p6.jpg',
+            price: 60,
+            countInstock: 0,
+            brand: 'Addidas',
+            rating: 4,
+            numReviews: 10,
+            description: 'hight quality product'
+        }
+    ]
+}
+
+export default data;
