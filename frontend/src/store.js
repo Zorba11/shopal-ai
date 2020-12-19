7fc8f7774cd92e422bf9c4049c3d4f3565d551ad
Zorba11 <zorbathegreek111@gmail.com>
2020-12-19 12:38:14 +0530

    added redux to HomeScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
-import {applyMiddleware, compose, createStore} from 'redux';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import data from "./data";
+import { productListReducers } from './reducers/productReducers';
 
 const initialState = {};
-const reducer = (state, action) => {
-    return {products: data.products};
-}
+const reducer = combineReducers({
+    productList: productListReducers,
+})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
